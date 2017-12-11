d8bf23ee166a0ddc19d4c3fcff897ca89ccbc32a
Sshanu <sshanukr@gmail.com>
2017-12-11 18:53:17 +0530

    update four word preprocessing

--- Four Word dataset/Preprocess_Files/viewkey.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-r = open("./Temp/tmpkey", "r").read().split()
-p = list(set(r))
-for i in range(0,len(p)):
-    print(sys.argv[1] + p[i] + "="+ str(r.count(p[i])))
--- Four Word dataset/Preprocess_Files/viewkey.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-sed 's/.*%//g' $1/$2/$3.key > Temp/tmpkey
-perl -pe 's|.*?:||' Temp/tmpkey > tmp2
-#perl -pe 's|.*?:||' tmp2 > Temp/tmpkey 
-rm tmp2
-#sed -i 's/:://g' Temp/tmpkey
-python viewkey.py $3
-cd ../
-
